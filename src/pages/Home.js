a8d915a37d57be0ccaaa4d1439a518464c43f3ad
LucaTeodorescu <lvteodorescu@gmail.com>
2023-07-28 01:39:38 +0200

    Home page css style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import "../styles/Home.css"
 
 function Home() {
     return(
         <div className="home">
-            <div className="about"></div>
+            <div className="about">
                 <h2> Hi, My Name is Luca</h2>
-                <div className="prompt"> <p> A Data Scientist with passion for learning and experimenting. </p> </div>
-        <div className="skills"></div>
+                <div className="prompt"> 
+                <p> A Data Scientist with passions for learning and experimenting. </p> 
+                </div>
+            </div>
+        <div className="skills">
+            <h1>Skills</h1>
+            <ol className="list">
+                <li className="item">
+                    <h2>Analysis</h2>
+                    <span>
+                        blablablabla
+                    </span>
+                    <h2>Computer Science</h2>
+                    <span>
+                        blablablabla
+                    </span>
+                </li>
+            </ol>
+        </div>
         </div>
     );
 }
