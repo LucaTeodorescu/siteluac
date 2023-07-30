424db810db60eb0652d8bef177b67e68cb601752
LucaTeodorescu <lvteodorescu@gmail.com>
2023-07-30 22:41:05 +0200

    Experience.js structure

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { 
+    VerticalTimeline, VerticalTimelineElement, 
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
 
 function Experience() {
     return(
-        <div>Experience</div>
+        <div className="experience">
+            <VerticalTimeline lineColor="#333333">
+                <VerticalTimelineElement 
+                className="vertical-timeline-element--education" 
+                date="2017-2019"
+                iconStyle={{background: "#333333", color: "#ffff"}}
+                icon={<SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">
+                        Classe Préparatoire Lycée Blaise Pascal MPSI - MP*
+                    </h3>
+
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
     );
 }
 
