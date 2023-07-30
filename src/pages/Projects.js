f5eb2739e0f469834255abfc1a8d871f50722ce6
LucaTeodorescu <lvteodorescu@gmail.com>
2023-07-30 22:55:20 +0200

    Displayed components in projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ProjectItem from '../components/Projectitem';
+import Proj1 from '../assets/dbfz.jpg';
+
+import '../styles/Project.css'
 
 function Projects() {
     return(
         <div className="projects">
-            <h1>My personnal project</h1>
+            <h1>My personnal projects :</h1>
             <div className="projectList">
+                <ProjectItem name="Social Media Website" image={Proj1} />
                 <ProjectItem />
             </div>
         </div>
